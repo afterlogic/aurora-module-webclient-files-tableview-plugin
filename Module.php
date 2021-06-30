62e12babe0561cfb9444f45efb700aad4e31aa38
sash04ek <a.ovcharov@gmail.com>
2021-06-30 12:00:08 +0300

    added laravel branch

--- Module.php
+++ Module.php
@@ -9,7 +9,7 @@ namespace Aurora\Modules\FilesTableviewWebclientPlugin;
 
 /**
  * Replaces folders and files list view template in Files module for displaying items in table.
- * 
+ *
  * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
@@ -18,27 +18,19 @@ namespace Aurora\Modules\FilesTableviewWebclientPlugin;
  */
 class Module extends \Aurora\System\Module\AbstractWebclientModule
 {
-	public function init() 
+	public function init()
 	{
-		\Aurora\Modules\Core\Classes\User::extend(
-			self::GetName(),
-			[
-				'EnableModule' => array('bool', false),
-				'EnablePreviewPane' => array('bool', false),
-			]
-
-		);
 	}
-	
+
 	/**
 	 * Obtains list of module settings for authenticated user.
-	 * 
+	 *
 	 * @return array
 	 */
 	public function GetSettings()
 	{
 		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::Anonymous);
-		
+
 		$oUser = \Aurora\System\Api::getAuthenticatedUser();
 		if (!empty($oUser) && $oUser->isNormalOrTenant())
 		{
@@ -47,29 +39,29 @@ class Module extends \Aurora\System\Module\AbstractWebclientModule
 				'EnablePreviewPane' => $oUser->{self::GetName().'::EnablePreviewPane'}
 			);
 		}
-		
+
 		return null;
-	}	
-	
+	}
+
 	/**
 	 * Updates settings of the Simple Chat Module.
-	 * 
+	 *
 	 * @param boolean $EnableModule indicates if user turned on Simple Chat Module.
 	 * @return boolean
 	 */
 	public function UpdateSettings($EnableModule, $EnablePreviewPane)
 	{
 		\Aurora\System\Api::checkUserRoleIsAtLeast(\Aurora\System\Enums\UserRole::NormalUser);
-		
+
 		$iUserId = \Aurora\System\Api::getAuthenticatedUserId();
 		if (0 < $iUserId)
 		{
 			$oUser = \Aurora\Modules\Core\Module::Decorator()->GetUserUnchecked($iUserId);
-			$oUser->{self::GetName().'::EnableModule'} = $EnableModule;
-			$oUser->{self::GetName().'::EnablePreviewPane'} = $EnablePreviewPane;
+			$oUser->setExtendedProp(self::GetName().'::EnableModule', $EnableModule);
+			$oUser->setExtendedProp(self::GetName().'::EnablePreviewPane', $EnablePreviewPane);
 			\Aurora\Modules\Core\Module::Decorator()->UpdateUserObject($oUser);
 		}
 		return true;
-	}	
-	
+	}
+
 }
