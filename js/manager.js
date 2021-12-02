f68021c9b6bc596bae39991c49dce5fd2f6240f7
nadine-shi <nadinshi@gmail.com>
2021-12-02 16:12:55 +0300

    refactor names

--- js/manager.js
+++ js/manager.js
@@ -42,12 +42,11 @@ module.exports = function (oAppData) {
 //				}
 				App.subscribeEvent('FilesWebclient::ShowView::after', function (oParams) {
 					var 
-						data = {
+						previewFileData = {
 							'displayName': ko.observable(''),
 							'fileInfo': ko.observable(''),
 							'enablePreviewPane': Settings.enablePreviewPane
 						},
-						oItem = null,
 						$RightPannel = $("<!-- ko template: {name: '%ModuleName%_PaneView'} --><!-- /ko -->"),
 						aImgMimeTypes = ['image/jpeg', 'image/png', 'image/gif']
 					;
@@ -58,17 +57,17 @@ module.exports = function (oAppData) {
 
 						$("#files_center_panel").after($RightPannel);
 
-						ko.applyBindings(data, $RightPannel.get(0));
-	
-						oParams.View.firstSelectedFile.subscribe(function(newValue) {
-							data.displayName('');
-							data.fileInfo('');
+						ko.applyBindings(previewFileData, $RightPannel.get(0));
+
+						oParams.View.firstSelectedFile.subscribe(function(selectedFile) {
+							previewFileData.displayName('');
+							previewFileData.fileInfo('');
 							$("#files_view_pane").html("");
-							if (newValue !== undefined && oItem !== newValue && Settings.enablePreviewPane())
+							if (selectedFile !== undefined && Settings.enablePreviewPane())
 							{
-								data.displayName(newValue.displayName());
-								data.fileInfo(newValue.sHeaderText);
-								if (typeof(newValue.oExtendedProps) !== 'undefined' &&  typeof(newValue.oExtendedProps.InitializationVector) !== 'undefined')
+								previewFileData.displayName(selectedFile.displayName());
+								previewFileData.fileInfo(selectedFile.sHeaderText);
+								if (typeof(selectedFile.oExtendedProps) !== 'undefined' &&  typeof(selectedFile.oExtendedProps.InitializationVector) !== 'undefined')
 								{
 									$("#files_view_pane").html("<span style=\"font-style: normal;\n\
 										font-weight: normal;\n\
@@ -81,13 +80,13 @@ module.exports = function (oAppData) {
 										font-family: 'afterlogic';\n\
 										width: 500px;\">&#59658;</span>");
 								}
-								else if (-1 !== $.inArray(newValue.mimeType(), aImgMimeTypes))
+								else if (-1 !== $.inArray(selectedFile.mimeType(), aImgMimeTypes))
 								{
-									$("#files_view_pane").html("<img style='width:100%;' src='" + newValue.getActionUrl('view') + "'>");
+									$("#files_view_pane").html("<img style='width:100%;' src='" + selectedFile.getActionUrl('view') + "'>");
 								}
 								else
 								{
-									$("#files_view_pane").html("<iframe id='view_iframe' name='view_iframe' style='width: 100%; height: 400px; border: none;' src='" + newValue.getActionUrl('view') + "'></iframe>");
+									$("#files_view_pane").html("<iframe id='view_iframe' name='view_iframe' style='width: 100%; height: 400px; border: none;' src='" + selectedFile.getActionUrl('view') + "'></iframe>");
 								}
 							}
 						});
